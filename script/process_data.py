847f055625284a5bf349c6172b2d7b79078e3022
cpwu <cpwu2017@126.com>
2025-04-11 13:49:40 +0800

    automatic config the device name

--- script/process_data.py
+++ script/process_data.py
@@ -1,5 +1,32 @@
 from param import FLAG_DICT
 
+import subprocess
+
+gpu_name = ''
+try:
+    # 调用 nvidia-smi 获取 GPU 名称和计算能力
+    output = subprocess.check_output(
+        ["nvidia-smi", "--id=0", "--query-gpu=name,compute_cap", "--format=csv,noheader"],
+        universal_newlines=True
+    ).strip()
+    gpu_name, gpu_cc = [s.strip() for s in output.split(',')]
+    gpu_name = gpu_name.replace(' ', '_')
+    print(f"GPU Name: {gpu_name}")
+    print(f"GPU Compute Capability: {gpu_cc}")
+    # 根据计算能力设置 CUDA_COMPUTE_CAPABILITY
+    if gpu_cc == "8.0":
+        BASE_FLAGS = '-O3 -arch sm_80 -DRUN_TIMES=1'
+    elif gpu_cc == "8.6":
+        BASE_FLAGS = '-O3 -arch sm_70 -DRUN_TIMES=1'
+    elif gpu_cc == "8.9":
+        BASE_FLAGS = '-O3 -arch sm_89 -DRUN_TIMES=1'
+    elif gpu_cc == "9.0":
+        BASE_FLAGS = '-O3 -arch sm_86 -DRUN_TIMES=1'
+    else:
+        raise RuntimeError(f"Unsupported GPU compute capability: {gpu_cc}")
+except Exception as e:
+    raise RuntimeError(f"Failed to detect GPU compute capability: {e}")
+
 
 def load_result(filename):
     with open(filename, 'r') as f:
@@ -44,13 +71,7 @@ def pick_best_csv(lines, filename):
 
 
 if __name__ == '__main__':
-    data_dir = './data/A100/layout16/'
-    config_csv = load_result(data_dir + 'ncu_result.txt')
-    store_csv(config_csv, data_dir + 'ncu_result_full.csv')
-    pick_tile_csv(config_csv, 1, data_dir + 'ncu_result_tile1.csv')
-    pick_best_csv(config_csv, data_dir + 'ncu_result_best.csv')
-
-    data_dir = './data/V100/layout16/'
+    data_dir = f'./data/{gpu_name}/layout16/'
     config_csv = load_result(data_dir + 'ncu_result.txt')
     store_csv(config_csv, data_dir + 'ncu_result_full.csv')
     pick_tile_csv(config_csv, 1, data_dir + 'ncu_result_tile1.csv')
